751853d2f97b6b375860bd193cdd38b75c1d874c
Adanosz <reti925@gmail.com>
2020-05-26 18:51:15 +0200

    fixed router

--- src/app/routers/flat.ts
+++ src/app/routers/flat.ts
@@ -3,7 +3,7 @@ import * as flatController from '../controllers/flat';
 
 export const router: Router = Router({ mergeParams: true });
 
-router.get('', flatController.index)
+router.get('/', flatController.index)
 router.get('/:id', flatController.show);
 router.post('/', flatController.create);
 router.put('/:id', flatController.update);
